45ad48edb715864dcade43fc0eb8917c9381d8e8
IiIymik <sambotemp@gmail.com>
2022-08-24 20:15:39 +0300

    add;

---
+++ funcs.py
@@ -0,0 +1,324 @@
+"""Personal functions and classes module
+
+Include all classes for personal assistant app
+"""
+
+from collections import UserDict
+from datetime import datetime
+import re
+
+
+class AddressBook(UserDict):
+
+    """The class inherits from UserDict class.
+
+    Creates <dict> for sroting adress book data.
+
+    Methods defined in the class are:
+
+    <add_record(self, name, value)>, takes contact name
+    and list of phones as a value parameter,
+    returns dict with new info as a key and value in it.
+
+    <iterator(self, n)>, method returns a record generator, takes <n> -
+    number of records in <self.data> that will be returned in
+    one generator call
+
+    <search_contacts(self, user_input)>, method searches contact's data
+    by the given from a user partial info
+    """
+
+    def add_record(self, name, value):
+        """Adds record with key <name> and value to it
+
+        Parameters
+        ----------
+        name: str
+        value: str
+
+        Returns
+        -------
+        self.data: dict
+        """
+        self.data[name] = value
+        return self.data
+
+    def iterator(self, num):
+        """Function returns <num> records from address_book
+
+        Parameters
+        ----------
+        num: int
+
+        Returns
+        -------
+        Info about result func has worked or None
+        """
+        info = False
+        if len(self.data) < num:
+            info = "Index is out or range"
+        else:
+            data_list = list(self.data.items())
+            while data_list:
+                result = "\n".join(
+                    [
+                        f'\n\tContact <{el[0]}> has following data:\
+                        \n\tbirthday info - {list(el[1].items())[0][1]}\
+                        \n\tphon/s - {"; ".join(list(el[1].items())[1][1])}'
+                        for el in data_list[:num]
+                    ]
+                )
+                yield result
+                data_list = data_list[num:]
+        return info or None
+
+    def search_contacts(self, user_input):
+        """Function search specified contact from address book
+
+        Parameters
+        ----------
+        user_input: str, phone number
+
+        Returns
+        -------
+        Contact data or None
+        """
+        info_list = []
+        for cont in self.data:
+            match_name_result = re.search(
+                user_input.casefold(), cont.casefold())
+            match_phone_result = re.search(
+                user_input, " ".join(self.data[cont]["phones"])
+            )
+            if match_name_result is not None:
+                info_list.append({cont: self.data[cont]})
+            elif match_phone_result is not None:
+                info_list.append({cont: self.data[cont]})
+        if len(info_list) == 0:
+            result = None
+        else:
+            cont_info = ""
+            for val in info_list:
+                for name, info_dict in val.items():
+                    cont_info += f'\n\tThe contact {name} has\
+        \n\tbirthday info - {list(info_dict.items())[0][1]}\
+        \n\tphone/s - {"; ".join(list(info_dict.items())[1][1])}.\n'
+            result = cont_info
+        return result
+
+    def __repr__(self):
+        """String representation method"""
+        result = ''
+        for key, value in self.data.items():
+            result += f'{key}={value}'
+        return result
+
+
+class Birthday:
+    """The class inherits from Fiel class.
+
+    Creates user's birthday(required attribute).
+    """
+
+    def __init__(self, birthday):
+        """Constructor Birthday class"""
+        self.__birthday = birthday
+
+    @property
+    def birthday(self):
+        """Function creates class's property with @property decorator"""
+        return self.__birthday
+
+    @birthday.setter
+    def birthday(self, birthday):
+        """Function-setter validates and assign
+        value to class's field
+
+        Parameters
+        ----------
+        birthday: str
+
+        Returns
+        -------
+        Info-string in case of not correct data or None
+        """
+        real_time = datetime.now()
+        datetime_dirthday = datetime.strptime(birthday, "%d.%m.%Y")
+        checking_age = real_time.year - datetime_dirthday.year
+        if checking_age >= 100:
+            result = "Hey, grandpa! You are too old) Check if \
+            you have entered correct birthday date."
+        elif real_time.year <= datetime_dirthday.year:
+            result = "Hey, baby! You are too young) Check if \
+            you have entered correct birthday date."
+        self.__birthday = birthday
+        return result or None
+
+    def birthday_year(self):
+        """Fuction returns the year of birth for contact"""
+        return self.__birthday.year
+
+
+class Name:
+
+    """The class inherits from Fiel class.
+
+    Creates user's name(required attribute) starts from an
+    uppercase letter with the following lowercase letters.
+    """
+
+    def __init__(self, name):
+        """Constructor for Name class"""
+        self.__name = None
+        self.name = name
+
+    @property
+    def name(self):
+        """Function creates class's property with @property decorator"""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """Function-setter validates and assign
+        value to class's field
+
+        Parameters
+        ----------
+        name: str
+
+        Returns
+        -------
+        Info-string in case of not correct data or None
+        """
+        result = False
+        if len(name.split(" ")) != 2:
+            result = "Enter fool name."
+        else:
+            name = name.casefold().title()
+            self.__name = name
+        return result or None
+
+    def introduse_instanse(self):
+        """Full form or instanse's name"""
+        return f"My name is {self.name}"
+
+
+class Phone:
+    """The class inherits from Fiel class.
+
+    Creates user's phone field(optional attribute).
+    """
+
+    def __init__(self, phone=None):
+        """Constructor for Phone class"""
+        if phone is None:
+            self.__phone = phone
+        else:
+            self.__phone = (
+                phone.strip()
+                .replace("(", "")
+                .replace(")", "")
+                .replace("-", "")
+                .replace(" ", "")
+            )
+
+    @property
+    def phone(self):
+        """Function creates class's property with @property decorator"""
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone):
+        """Function-setter validates and assign
+        value to class's field
+
+        Parameters
+        ----------
+        phone: str
+
+        Returns
+        -------
+        None
+        """
+        phone = (
+            phone.strip()
+            .replace("(", "")
+            .replace(")", "")
+            .replace("-", "")
+            .replace(" ", "")
+        )
+        self.__phone = phone
+
+    def get_network(self):
+        """Simple func to return network's code"""
+        return self.phone[2:5]
+
+
+class Record:
+    """The class for creating fields for phone book.
+
+    Methods defined in the class:
+
+    <add_contact_phonenumb(self, phone_numb)> - creats new contact's
+    phone and appends to other contact's phones stored inside a list
+    defined inside constructor while creating class instance,
+
+    <days_to_birthday(self, birthday)> - counts amount of days till
+    following birthday if <birthday != None>
+    """
+
+    def __init__(self, birthday=None):
+        """Constructor for Phone class"""
+        self.birthday = birthday
+        self.phones = []
+
+    def add_contact_phonenumb(self, phone_numb):
+        """Function validates entered phone number
+        and adds to contact's list of phones
+
+        Parameters
+        ----------
+        phone_numb: str
+
+        Returns
+        -------
+        list of phones
+        """
+        phone_numb = str(
+            phone_numb.strip()
+            .replace("(", "")
+            .replace(")", "")
+            .replace("-", "")
+            .replace(" ", "")
+        )
+        self.phones.append(phone_numb)
+        return self.phones
+
+    def days_to_birthday(self):
+        """Function calculates amount of days till following birthday
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        amount of days till birthday or info-string
+        """
+        result = False
+        if self.birthday is not None:
+            real_time = datetime.now()
+            datetime_dirthday = datetime.strptime(self.birthday, "%d.%m.%Y")
+            this_year_birthday = datetime_dirthday.replace(year=real_time.year)
+            next_year_birthday = datetime_dirthday.replace(
+                year=real_time.year + 1)
+            this_year_days_count = (this_year_birthday - real_time).days
+            next_year_days_count = (next_year_birthday - real_time).days
+            result = (
+                this_year_days_count
+                if real_time.date() < this_year_birthday.date()
+                else next_year_days_count
+            )
+            result = (
+                f"\n\tThere are {result} days left until the contact's next birthday"
+            )
+        return result or "\n\tThere is no birthday date for contact"
---
+++ test_main.py
@@ -0,0 +1,331 @@
+"""Personal assistant test module
+
+The script makes tests for personal assistant's
+classes, functions and methods
+"""
+
+import pytest
+from main import (
+    inner_parser,
+    add_phone,
+    deserialize_data,
+    parsing_user_input,
+    handler_func,
+    show_one_contact_data,
+    serialize_data,
+    change_existing_phone,
+    save_and_exit,
+    show_all_contacts,
+    main)
+from funcs import Name, Phone, Record, AddressBook, Birthday
+
+
+def test_inner_parser():
+    """Inner_parser test func"""
+    data = ('Romanskyy Andrey', '380675119811')
+    assert inner_parser(data) == (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+
+
+def test_add_phone():
+    """Test-func for <add_phone> function"""
+    address_book = AddressBook()
+    name, phone = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record = Record()
+    record.add_contact_phonenumb(phone)
+    address_book.add_record(name, record.__dict__)
+
+    datacase_one = ('Zivenko Kos', '380675119812')
+    result_one = (
+        "\n\tThe contact <Zivenko Kos> was added to address book with phone <380675119812>."
+    )
+
+    datacase_two = ('Romanskyy Andrey', '380675119811')
+    result_two = (
+        "\n\tThe contact's data is already in address book."
+    )
+
+    datacase_three = ('Romanskyy Andrey', '380675119822')
+    result_three = (
+        "\n\tThe phone <380675119822> for contact <Romanskyy Andrey>\n\t\
+        was successfully added to address book."
+    )
+
+    assert add_phone(address_book, datacase_one) == result_one
+    assert add_phone(address_book, datacase_two) == result_two
+    assert add_phone(address_book, datacase_three) == result_three
+
+
+@pytest.fixture
+def empty_dict():
+    """Fixture for <test_deserialize_data> function"""
+    some_dict = {"test": 1}
+    return some_dict
+
+
+def test_deserialize_data(empty_dict: dict):
+    """Test-func for <deserialize_data> function"""
+    path = 'test_data.bin'
+    serialize_data(empty_dict, path)
+    assert deserialize_data(path) == empty_dict
+
+
+@pytest.fixture
+def wrong_dict():
+    """Fixture for <deserialize_data> function with wrong data"""
+    wrong_dict = {"wrong_test": 2}
+    return wrong_dict
+
+
+def test_wrong_deserialize_data(empty_dict: dict, wrong_dict: dict):
+    """Test-func for <deserialize_data> function with wrong data"""
+    path = 'test_wrong_data.bin'
+    serialize_data(empty_dict, path)
+    assert deserialize_data(path) != wrong_dict
+
+
+def test_parsing_user_input():
+    """Test-func for <parsing_user_input> function"""
+    correct_input = 'add Andrey Romanskyy 380675119811'
+    bad_phone = 'add Andrey Romanskyy 876876876876876'
+    bad_name = 'add AndreyRomanskyy 380675119811'
+    incorrect_input = 'some lsoij lkjlkj'
+
+    assert parsing_user_input(correct_input) == (
+        'add', ('Andrey Romanskyy', '380675119811'))
+    assert parsing_user_input(bad_phone) == (
+        'add', ('Andrey Romanskyy', False))
+    assert parsing_user_input(bad_name) == (
+        'add', (False, '380675119811'))
+    assert parsing_user_input(incorrect_input) == (
+        (False, ('Lsoij Lkjlkj', False))
+    )
+
+
+def test_search_contacts():
+    """Test-func for <search_contacts> function"""
+    address_book = AddressBook()
+    name, phone = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record = Record()
+    record.add_contact_phonenumb(phone)
+    address_book.add_record(name, record.__dict__)
+
+    contact_data = 'romans'
+    wrong_data = 'some'
+
+    result = '\n\tThe contact Romanskyy Andrey has\
+        \n\tbirthday info - None\
+        \n\tphone/s - 380675119811.\n'
+
+    assert address_book.search_contacts(contact_data) == result
+    assert address_book.search_contacts(wrong_data) != result
+    assert address_book.search_contacts(wrong_data) is None
+
+
+def test_handler_func():
+    """Test-func for <handler_func> function"""
+    address_book = AddressBook()
+    name, phone = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record = Record()
+    record.add_contact_phonenumb(phone)
+    address_book.add_record(name, record.__dict__)
+
+    show_command = 'show all'
+    show_result = "\n\tYou have seen all contact's info."
+
+    data = ('Nata Bazan', '380675554433')
+    add_command = 'add'
+    add_result = '\n\tThe contact <Nata Bazan> was added to address book with phone <380675554433>.'
+
+    assert handler_func(show_command, address_book, data) == (
+        '\n\tContinue your work with commands.'
+    )
+    assert handler_func(show_command, address_book, data) == show_result
+    assert handler_func(show_command, address_book, data) == show_result
+    assert handler_func(add_command, address_book, data) == add_result
+
+
+def test_show_one_contact_data():
+    """Test-func for <show_one_contact_data> function"""
+    address_book = AddressBook()
+    name, phone = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record = Record()
+    record.add_contact_phonenumb(phone)
+    address_book.add_record(name, record.__dict__)
+
+    result = '\n\tThe contact Romanskyy Andrey has\
+        \n\tbirthday info - None\
+        \n\tphone/s - 380675119811.\n'
+
+    wrong_result = None
+
+    assert show_one_contact_data(address_book) == result
+    assert show_one_contact_data(address_book) == wrong_result
+
+
+def test_serislize_data():
+    """Test-func for <serislize_data> function"""
+    example_data = {'some': 10}
+    path = 'some.bin'
+
+    assert serialize_data(example_data, path) is None
+
+
+def test_get_network():
+    """Test-func for <get_network> method"""
+    raw_num = '3(8067)511-98-11'
+    result = '067'
+    assert Phone(raw_num).get_network() == result
+
+
+def test_add_contact_phonenumber():
+    """Test-func for <add_contact_phonenumber> method"""
+    record = Record()
+    record.add_contact_phonenumb('3(8067)511-98-11')
+    result = ['380675119811']
+    assert record.phones == result
+
+
+def test_days_to_birthday():
+    """Test-func for <days_to_birthday> method"""
+    rom = Record('03.06.1978')
+    assert rom.days_to_birthday() == (
+        "\n\tThere are 214 days left until the contact's next birthday"
+    )
+
+
+def test_add_record():
+    """Test-func for <add_record> method"""
+    assert AddressBook().add_record(
+        'Andrey Romanskyy', '380575119811'
+    ) == {'Andrey Romanskyy': '380575119811'}
+
+    assert AddressBook().add_record(
+        'Andrey Romanskyy', '380575119811'
+    ) != 'Andrey Romanskyy=380575119811'
+
+
+def test_birthday():
+    """Test-func for <birthday> field"""
+    assert Birthday('03.06.1978').birthday == '03.06.1978'
+
+
+def test_name():
+    """Test-func for <name> field"""
+    assert Name('Romanskyy Andrey').name == 'Romanskyy Andrey'
+
+
+def test_introduse_instanse():
+    """Test-func for <introduse_instanse> method"""
+    assert Name('Romanskyy Andrey').introduse_instanse() == (
+        'My name is Romanskyy Andrey'
+    )
+
+
+def test_record():
+    """Test-func for <Record> class"""
+    rec = Record('03.06.1978')
+    rec.add_contact_phonenumb('38(0676665544)')
+    rec.add_contact_phonenumb('38(0676)665--544)')
+    assert rec.birthday == '03.06.1978'
+    assert rec.phones == ['380676665544', '380676665544']
+
+
+def test_change_existing_phone():
+    """Test-func for <change_existing_phone> method"""
+    address_book = AddressBook()
+    name, phone = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record = Record()
+    record.add_contact_phonenumb(phone)
+    address_book.add_record(name, record.__dict__)
+
+    data = ('Romanskyy Andrey', '380675119811')
+    assert change_existing_phone(address_book, data) == (
+        "\n\t<380675119811> for contact <Romanskyy Andrey>\n\twas changed for <380675119812>."
+    )
+
+
+def test_change_error():
+    """Test-func for <change_existing_phone> method"""
+    address_book = AddressBook()
+    name, phone = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record = Record()
+    record.add_contact_phonenumb(phone)
+    address_book.add_record(name, record.__dict__)
+
+    data = ('Romanskyy Andrey', '380675119811')
+
+    assert change_existing_phone(address_book, data) == (
+        '\n\tEnter correct number.'
+    )
+
+
+def test_exit():
+    """Test-func for <exit> func"""
+    address_book = AddressBook()
+    name, phone = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record = Record()
+    record.add_contact_phonenumb(phone)
+    address_book.add_record(name, record.__dict__)
+
+    with pytest.raises(SystemExit) as pytest_exit:
+        save_and_exit(address_book)
+    assert pytest_exit.type == SystemExit
+    assert pytest_exit.value.code == (
+        "\n\tThe address book was saved to <data.bin> file.\n\tGood bye!"
+    )
+
+
+def test_show_all_contacts():
+    """Test-func for <show_all_contacts> func"""
+    address_book = AddressBook()
+
+    name_one, phone_one = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record_one = Record()
+    record_one.add_contact_phonenumb(phone_one)
+    address_book.add_record(name_one, record_one.__dict__)
+
+    name_two, phone_two = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record_two = Record()
+    record_two.add_contact_phonenumb(phone_two)
+    address_book.add_record(name_two, record_two.__dict__)
+
+    # use <n>
+    assert show_all_contacts(address_book) == (
+        '\n\tContinue your work with commands.'
+    )
+
+    # use <y>
+    with pytest.raises(StopIteration) as pytest_si:
+        show_all_contacts(address_book)
+    assert pytest_si.type == StopIteration
+    # assert pytest_exit.value.code == (
+    #     "\n\tThe address book was saved to <data.bin> file.\n\tGood bye!"
+    # )
+
+
+def test_main():
+    """Test-func for <main> func"""
+    address_book = AddressBook()
+
+    name_one, phone_one = (
+        Name('Romanskyy Andrey').name, Phone('380675119811').phone)
+    record_one = Record()
+    record_one.add_contact_phonenumb(phone_one)
+    address_book.add_record(name_one, record_one.__dict__)
+
+    with pytest.raises(SystemExit) as pytest_main:
+        main()
+    assert pytest_main.type == SystemExit
+    assert pytest_main.value.code == (
+        "\n\tThe address book was saved to <data.bin> file.\n\tGood bye!"
+    )
